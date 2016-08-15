28894018f553f76eace9e526fa19e78ecb8882a4
Carlos Romarate <cromarate@lancera.com>
2016-08-15 14:54:46 +0800

    Initial Commit

---
+++ Pokemon Heldesk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon Heldesk
+//
+//  Created by Carlos Romarate Jr on 15/08/2016.
+//  Copyright © 2016 Carlos Romarate Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
